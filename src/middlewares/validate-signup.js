1285767ce1315f276b792ad1a0970d1cbd5090d0
Daniel Austin <danielaustin718@gmail.com>
2020-05-09 16:04:30 +0100

    Check if email has already been taken by another user

--- src/middlewares/validate-signup.js
+++ src/middlewares/validate-signup.js
@@ -1,4 +1,5 @@
 import { check } from 'express-validator';
+import { User } from '../models';
 
 export default [
   check('displayName')
@@ -7,12 +8,25 @@ export default [
     .withMessage('Display name is required')
     .custom(value => /^[a-zA-Z_\-0-9]+( [a-zA-Z_\-0-9]+)*$/.test(value))
     .withMessage('Display name can only contain characters a-z, A-Z, 0-9, _, -, or a whitespace.'),
+
   check('email')
     .trim()
     .exists({ checkFalsy: true, checkNull: true })
     .withMessage('Email address is required')
     .isEmail()
-    .withMessage('Email address is not valid'),
+    .withMessage('Email address is not valid')
+    .custom(email =>
+      User.findOne({ email })
+        .then(user => {
+          if (user) {
+            return Promise.reject();
+          } else {
+            return Promise.resolve();
+          }
+        })
+    )
+    .withMessage('Email address already used by another user'),
+
   check('password')
     .trim()
     .exists({ checkFalsy: true, checkNull: true })
