f8964c09129626b1e5e64fd6a86a2037ed5939f0
Daniel Austin <danielaustin718@gmail.com>
2020-05-11 11:03:42 +0100

    Validate search query

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -4,3 +4,4 @@ export { default as validateLogin } from './validate-login';
 export { default as validateSignup } from './validate-signup';
 export { default as validateAnswer } from './validate-answer';
 export { default as validateQuestion } from './validate-question';
+export { default as validateSearchQuery } from './validate-search-query';
